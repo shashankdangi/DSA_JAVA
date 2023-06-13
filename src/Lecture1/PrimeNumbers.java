5e7428bed833cfa85f3e2a11a09d37317ca626bf
Shashank Dangi <91678732+shashankdangi@users.noreply.github.com>
2023-06-13 13:47:46 +0530

    Lecture 1

---
+++ src/Lecture1/PrimeNumbers.java
@@ -0,0 +1,20 @@
+package Lecture1;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int n = 6;
+        int i = 2;
+        int cout = 0;
+        while(i<n){
+            if(n%i  == 0){
+                cout += 1;
+            }
+            i++;
+        }
+        if(cout>1){
+            System.out.println("Not Prime");
+        }else{
+            System.out.println(" Prime");
+        }
+    }
+}
