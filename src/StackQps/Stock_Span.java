8f1db994a7661727ae7303b029f87bb94ee6be7c
shashankdangi <sss12212001@outlook.com>
2023-08-17 23:38:03 +0530

    Stack Ques

---
+++ src/StackQps/Stock_Span.java
@@ -0,0 +1,14 @@
+package StackQps;
+
+public class Stock_Span {
+    public static void main(String[] args) {
+        int[] arr = {91,11,13,15,38,42,37};
+        SpanCal(arr);
+    }
+    public static void SpanCal(int[] arr){
+        int[] span = new int[arr.length];
+        for(int  i = 0 ; i<arr.length; i++){
+
+        }
+    }
+}
